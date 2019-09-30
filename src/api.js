b2c089b8f77cbf86a53c8eb364e58116eb924e8a
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-30 09:05:50 -0600

    part one and two complete

--- src/api.js
+++ src/api.js
@@ -1,6 +1,20 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+// const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
 
-const getItems() {
+// const getItems() {
+//   return Promise.resolve('A successful response!');
+// }
+
+// export default {
+//   getItems
+// };
+
+
+
+
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Shannon';
+
+function getItems(){
   return Promise.resolve('A successful response!');
 }
 
