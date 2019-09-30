484e6c2da374faff833adb8dbffffafb1b08a3b4
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-30 10:18:04 -0600

    number five done

--- src/index.js
+++ src/index.js
@@ -5,21 +5,17 @@ import './index.css';
 
 import api from './api';
 import shoppingList from './shopping-list';
+import store from './store';
 
 
 
 const main = function () {
-  api.createItem('pears')
-    .then(res => res.json())
-    .then((newItem) => {
-      return api.getItems();
-    })
+  api.getItems()
     .then(res => res.json())
     .then((items) => {
-      console.log(items);
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
     });
-  console.log(api.BASE_URL);
-
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
